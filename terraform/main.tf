9faa5cdbeb1da017be9a232160c0c493669812b8
RalphBrynard <ralph@thebrynards.com>
2021-12-08 22:25:01 -0500

    vpc peering

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,10 +53,11 @@ module "vpc-peering_example_multi-account-multi-region" {
   version = "4.0.1"
   # insert the 6 required variables here
 
-  providers = {
-    aws.this = aws.this
-    aws.peer = aws.peer
-  }
+  aws_peer_access_key = var.aws_peer_access_key
+  aws_peer_secret_key = var.aws_peer_secret_key
+
+  aws_this_access_key = var.aws_this_access_key
+  aws_this_secret_key = var.aws_this_secret_key
 
   this_vpc_id = data.terraform_remote_state.vpc.vpc_id
   peer_vpc_id = data.terraform_remote_state.vpc.vpc_id
